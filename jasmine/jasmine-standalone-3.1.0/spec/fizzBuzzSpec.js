003ab3f59abadb301369f221b29a4167431aaffc
Patryk Pilecki <ppilecki@icloud.com>
2018-04-16 17:52:54 +0100

    fizzbuzz + tests

--- jasmine/jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -1,6 +1,12 @@
 describe("fizzBuzz", function() {
 
-  it('should print a range of numbers', function() {
-    expect(fizzBuzz(1,5)).toEqual("12345");
+  it('prints fizz for three', function() {
+    expect(fizzBuzz(1,3)).toEqual("12Fizz");
+  })
+  it('prints buzz for five', function() {
+    expect(fizzBuzz(4,5)).toEqual("4Buzz");
+  })
+  it('prints fizzbuzz for fifteen', function() {
+    expect(fizzBuzz(14,15)).toEqual("14FizzBuzz");
   })
 });
--- jasmine/jasmine-standalone-3.1.0/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-3.1.0/src/fizzBuzz.js
@@ -1,7 +1,14 @@
 function fizzBuzz(start, end) {
   var message = "";
   for (var i = start; i <= end; i++) {
-    message += i;
+    if (i % 15 === 0)
+       message += "FizzBuzz"
+    else if (i % 3 === 0)
+       message += "Fizz"
+    else if (i % 5 === 0)
+       message += "Buzz"
+    else
+       message += i
   }
   return message;
 };
