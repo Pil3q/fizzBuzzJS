0fcc990643b7e1e09b9a9fa371224b2356d52300
Andrew Kemp <akemp850@gmail.com>
2018-04-16 16:41:05 +0100

    Added initial test

---
+++ jasmine/jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -0,0 +1,6 @@
+describe("fizzBuzz", function() {
+
+  it('should print a range of numbers', function() {
+    expect(fizzBuzz(1,5)).toEqual("12345");
+  })
+});
---
+++ jasmine/jasmine-standalone-3.1.0/src/fizzBuzz.js
@@ -0,0 +1,7 @@
+function fizzBuzz(start, end) {
+  var message = "";
+  for (var i = start; i <= end; i++) {
+    message += i;
+  }
+  return message;
+};
